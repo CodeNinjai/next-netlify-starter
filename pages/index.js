3f5ea57f3b1458d648456c8b7faa2e6337a5773a
Jan Leimbach <jan.leimbach@outlook.com>
2022-02-09 13:44:57 +0100

    feature: added random background images

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,34 @@ import Header from '@components/Header'
 import React, { useState, useEffect } from "react";
 import moment from 'moment';
 
+let isBackgroundSet = false;
+
 export default function Home() {
+
+  useEffect(() => {
+    let frame_set = [
+      'fuerte1.jpg',
+      'fuerte2.jpg',
+      'fuerte3.jpg',
+      'fuerte4.jpg',
+      'fuerte5.jpg',
+      'fuerte6.jpg',
+      'fuerte7.jpg',
+      'fuerte8.jpg',
+      'fuerte9.jpg',
+      'fuerte10.jpg',
+      'fuerte11.jpg',
+      'fuerte12.jpg'
+    ];
+
+    if (!isBackgroundSet) {
+      let frame_color = Math.floor((Math.random() * (frame_set).length));
+      let url_p = "url(\"" + frame_set[frame_color] + "\")";
+      document.body.style.backgroundImage = url_p;
+      isBackgroundSet = true;
+    }
+  });
+
 
   const [duration, setDuration] = useState();
 
@@ -12,6 +39,7 @@ export default function Home() {
       setDuration(durationAsString('2022-03-17 11:25:00'));
     }, 1000);
   });
+
 
   return (
     <div className="container">
